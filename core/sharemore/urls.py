1ef157dfb4bd972fb7a43d5f3bd05fdadaa8a82a
Arthur Kay <14153403+AEarth@users.noreply.github.com>
2023-10-09 14:00:37 -0400

    commit push to github

---
+++ core/sharemore/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from core.views import frontpage, about, base #FrontpageView, AboutView
+
+urlpatterns = [
+
+    path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
+    path('base/', base, name='base'),
+    path('', include('userprofile.urls')),
+    path('', include('store.urls')),
+    path('', frontpage, name='frontpage'),
+]
+
+# if settings.enviro_set == 'local':
+#     urlpatterns.insert(3, path("__debug__/", include("debug_toolbar.urls")),
+
+
+
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
