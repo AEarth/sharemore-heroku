f61e8fd9d6572c3b0fa6efccc46c35eb2a327723
Arthur Kay <14153403+AEarth@users.noreply.github.com>
2023-11-22 19:14:56 -0500

    tw/dui theme, django-filter, search, view pref session var

--- sharemore/settings.py
+++ sharemore/settings.py
@@ -18,7 +18,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#ENVIRO_SET = 'local'
+ENVIRO_SET = '' #'local'
 
 env = environ.Env(
     # set casting, default value
